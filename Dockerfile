4ecd6e38fd18245883b3f04c396c68eea876c825
Azize Elamrani <azize.elamrani@gmail.com>
2019-11-21 17:47:42 +0100

    --wip-- [skip ci]

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM markhobson/maven-chrome
-
-ENV CHROME_BIN /opt/google/chrome/google-chrome
-
-COPY ./ /tmp/
-WORKDIR /tmp/
-
-RUN mvn clean deploy
--- karma.conf.js
+++ karma.conf.js
@@ -1,3 +1,4 @@
+
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
@@ -8,9 +9,10 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
+      require('karma-phantomjs-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -25,7 +27,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome', 'ChromeHeadlessCustom'],
+    browsers: ['PhantomJS', 'Chrome', 'ChromeHeadlessCustom'],
     customLaunchers: {
       ChromeHeadlessCustom: {
         base: 'ChromeHeadless',
