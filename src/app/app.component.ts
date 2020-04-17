67fda01f8187b2018f1630f649d48008e77883e9
Azize Elamrani <azize.elamrani@gmail.com>
2020-04-17 16:51:54 +0200

    fix: manage my api-keys
    fix gravitee-io/issues#3483

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -121,6 +121,7 @@ export class AppComponent implements AfterViewInit, OnInit, OnDestroy {
       this.currentUser = newCurrentUser;
       this.userRoutes = this.navRouteService.getUserNav();
       if (this.currentUser) {
+        this.loadNotifications();
         this.interval = setInterval(
           () => {
             this.loadNotifications();
