aa808f6130a68857e462c9d370b71feab57b4731
Guillaume Cusnieux <guillaume.cusnieux@graviteesource.com>
2020-05-13 18:55:40 +0200

    fix: resolve display of homepage when resize window
    fix gravitee-io/issues#3751

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -221,11 +221,6 @@ export class AppComponent implements AfterViewInit, OnInit, OnDestroy {
     this.computeMenuMode();
   }
 
-  @HostListener('window:resize')
-  onResize() {
-    this.computeHomepageHeight();
-  }
-
   private computeMenuMode() {
     const pageYOffset = window.pageYOffset;
     window.requestAnimationFrame(() => {
